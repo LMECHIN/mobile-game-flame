6e5e0f646561a81d985443cbc972ddea928e0648
MECHIN <lmechin@economiedenergie.fr>
2024-02-28 17:50:56 +0100

    [EDIT] adjust all lux

---
+++ assets/tiles/neon_effect_45.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="neon_effect_45" tilewidth="260" tileheight="264" tilecount="1" columns="1">
+ <image source="../images/Sprites/14-TileSets/neon_effect_45.png" width="260" height="265"/>
+</tileset>
